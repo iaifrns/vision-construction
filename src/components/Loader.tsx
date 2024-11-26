0e53528035ef2a149d0b226d1e8fbab18299141f
iaifrns <nsinifranc@gmail.com>
2024-11-26 11:38:15 +0100

    finished with the loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react/dist/iconify.js'
 const Loader = () => {
   return (
     <div className='h-screen w-full flex justify-center items-center'>
-      <Icon icon="bx:loader-circle" className='text-primary text-[32px] animate-spin' />
+      <Icon icon="bx:loader-circle" className='text-primary text-[52px] animate-spin' />
     </div>
   )
 }
